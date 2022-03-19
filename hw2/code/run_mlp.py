fe382ab9406aab530fad4df53126f75404b10e48
sanglee <pksll325@gmail.com>
2022-03-20 06:09:52 +0900

    hw2 done

--- hw2/code/run_mlp.py
+++ hw2/code/run_mlp.py
@@ -8,6 +8,9 @@ from pytorch_lightning.loggers import TensorBoardLogger
 
 from modules import FeatureDataModule, MlpClassifier
 
+import seaborn
+import matplotlib.pyplot as plt
+
 
 def parse_args(argv=None):
     parser = ArgumentParser(__file__, add_help=False)
@@ -21,6 +24,32 @@ def parse_args(argv=None):
         osp.join(osp.dirname(__file__), '../data/mlp')))
     args = parser.parse_args(argv)
     return args
+ 
+ 
+def plot_confusion_matrix(data, labels, output_filename):
+    """Plot confusion matrix using heatmap.
+ 
+    Args:
+        data (list of list): List of lists with confusion matrix data.
+        labels (list): Labels which will be plotted across x and y axis.
+        output_filename (str): Path to output file.
+ 
+    """
+    seaborn.set(color_codes=True)
+    plt.figure(1, figsize=(9, 6))
+ 
+    plt.title("Confusion Matrix")
+ 
+    seaborn.set(font_scale=1.4)
+    ax = seaborn.heatmap(data, annot=True, cmap="YlGnBu", cbar_kws={'label': 'Scale'})
+ 
+    ax.set_xticklabels(labels)
+    ax.set_yticklabels(labels)
+ 
+    ax.set(ylabel="True Label", xlabel="Predicted Label")
+ 
+    plt.savefig(output_filename, bbox_inches='tight', dpi=300)
+    plt.close()
 
 
 def main(args):
@@ -42,6 +71,29 @@ def main(args):
     prediction_path = osp.join(logger.log_dir, 'test_prediction.csv')
     df.to_csv(prediction_path, index=False)
     print('Output file:', prediction_path)
+    
+
+    nb_classes = 15
+    val_loader = data_module.val_dataloader()
+    print(val_loader)
+    confusion_matrix = torch.zeros(nb_classes, nb_classes)
+    
+    with torch.no_grad():
+        for i, (inputs, classes) in enumerate(val_loader):
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            for t, p in zip(classes.view(-1), preds.view(-1)):
+                confusion_matrix[t.long(), p.long()] += 1
+
+    print(confusion_matrix)
+
+    confusion_path = osp.join(logger.log_dir, 'confmat.png')
+    # define data
+    data = confusion_matrix.tolist()
+    # define labels
+    labels = [i for i in range(0,15)]  
+    # create confusion matrix
+    plot_confusion_matrix(data, labels, confusion_path)
 
 
 if __name__ == '__main__':
--- hw2/script.sh
+++ hw2/script.sh
@@ -3,3 +3,36 @@ python code/run_sift.py data/labels/debug.csv --debug
 
 # generate SIFT for train_val data
 python code/run_sift.py data/labels/train_val.csv
+
+# generate SIFT for test data
+python code/run_sift.py data/labels/test_for_students.csv
+
+
+# debug k_means with train
+python code/train_kmeans.py data/labels/debug.csv data/sift/ 128 sift_128 --debug
+
+# train k_means with train
+python code/train_kmeans.py data/labels/train_val.csv data/sift/ 128 sift_128
+
+
+# debug bow with train
+python code/run_bow.py data/labels/debug.csv sift_128 data/sift --debug
+
+# get bow with train_val
+python code/run_bow.py data/labels/train_val.csv sift_128 data/sift
+python code/run_bow.py data/labels/test_for_students.csv sift_128 data/sift
+
+
+# extract CNN features
+python code/run_cnn.py data/labels/debug.csv --debug
+
+# get CNN features
+python code/run_cnn.py data/labels/train_val.csv
+python code/run_cnn.py data/labels/test_for_students.csv
+
+python code/run_cnn.py data/labels/train.csv
+python code/run_cnn.py data/labels/eval.csv
+
+# bow mlp
+python code/run_mlp.py sift --feature_dir data/bow_sift_128 --num_features 128
+python code/run_mlp.py cnn --feature_dir data/cnn-eval --num_features 512
